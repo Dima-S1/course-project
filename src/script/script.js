655ec993791f2e8ec3ee53ffc96102ee48cda8e1
Dima-S1 <dmitry.serov8@gmail.com>
2020-12-24 14:05:39 +0300

    Сделано наименование по БЭМ

--- src/script/script.js
+++ src/script/script.js
@@ -2,7 +2,7 @@ let mySwiper = new Swiper('.swiper-container', {
 	loop: true,
 });
 
-let gallerySwiper = new Swiper('.gallery-swiper-container', {
+let gallerySwiper = new Swiper('.gallery__swiper-container', {
 	loop: true,
   navigation: {
 	 nextEl: '.swiper-button-next',
@@ -22,33 +22,33 @@ const choices = new Choices(element, {
 
 /* Заливка иконки поиска*/
 
-const rect = document.querySelector('.header-bot-search__btn svg rect');
-const path = document.querySelector('.header-bot-search__btn svg path');
-const input = document.querySelector('.header-bot-search__input');
+const rect = document.querySelector('.header-search__btn svg rect');
+const path = document.querySelector('.header-search__btn svg path');
+const input = document.querySelector('.header-search__input');
 
 input.onfocus = function() {
-	rect.classList.add('header-bot-search__bth--fill');
-	path.classList.add('header-bot-search__bth--fill')
+	rect.classList.add('header-search__bth--fill');
+	path.classList.add('header-search__bth--fill')
 }
 
 input.onblur = function() {
-	rect.classList.remove('header-bot-search__bth--fill');
-	path.classList.remove('header-bot-search__bth--fill')
+	rect.classList.remove('header-search__bth--fill');
+	path.classList.remove('header-search__bth--fill')
 }
 
 /* Header выпадающий список*/
 
-let el = document.getElementsByClassName('header-bot-item');
+let el = document.getElementsByClassName('header-bot__item');
 
 for (let i = 0; i < el.length; i++) {
 	el[i].addEventListener('click', toggleMenu, false);
 }
 
 function toggleMenu() {
-	this.children[0].classList.toggle('is-open--title');
+	this.children[0].classList.toggle('show--title');
 	this.children[0].classList.toggle('after');
 	if(this.children.length > 1) {
-		this.children[1].classList.toggle('is-open--list');
+		this.children[1].classList.toggle('show--list');
 	} else {
 		return false;
 	}
