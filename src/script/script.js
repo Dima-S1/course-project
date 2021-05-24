3bdbde082bd7f45f864568dcca63e4b2e4504ec5
Dima-S1 <dmitry.serov8@gmail.com>
2021-05-24 19:17:59 +0300

    Правки в Header и новая функция выпадающего меню

--- src/script/script.js
+++ src/script/script.js
@@ -11,31 +11,50 @@ document.querySelector('.header-search-open-close__btn').addEventListener('click
 // Функция открытия и закрытия меню бургер
 
 document.querySelector('.header-burger').addEventListener('click', function() {
-  document.querySelector('.header-burger-menu').classList.add('is-open_block');
+  document.querySelector('.header-nav_burger').classList.add('is-open_block');
 });
 
-document.querySelector('.header-burger-menu-close').addEventListener('click', function() {
-  document.querySelector('.header-burger-menu').classList.remove('is-open_block');
+document.querySelector('.header-search-nav-close__btn_burger').addEventListener('click', function() {
+  document.querySelector('.header-nav_burger').classList.remove('is-open_block');
 });
 
 // Выпадающий список
 
-document.querySelector('#dropbtn1').addEventListener('click', function() {
-  document.querySelector('#droplist1').classList.toggle('is-open_block');
+const button = document.querySelectorAll('.header-menu-item-link');
+const drop = document.querySelectorAll('.header-dropdown');
+
+button.forEach(el => {
+  el.addEventListener('click', (e) => {
+    drop.forEach(el => {el.classList.remove(('is-open_block'))})
+    e.currentTarget.closest('li').querySelector('.header-dropdown').classList.toggle('is-open_block');
+  });
 });
 
-document.querySelector('#dropbtn2').addEventListener('click', function() {
-  document.querySelector('#droplist2').classList.toggle('is-open_block');
+document.addEventListener('click', (e) => {
+  if (!e.target.classList.contains('header-dropdown') && !e.target.classList.contains('header-menu-item-link')) {
+    drop.forEach(el => {el.classList.remove(('is-open_block'))})
+  }
 });
 
-document.querySelector('#dropbtn3').addEventListener('click', function() {
-  document.querySelector('#droplist3').classList.toggle('is-open_block');
-});
 
-document.querySelector('#dropbtn4').addEventListener('click', function() {
-  document.querySelector('#droplist4').classList.toggle('is-open_block');
-});
 
-document.querySelector('#dropbtn5').addEventListener('click', function() {
-  document.querySelector('#droplist5').classList.toggle('is-open_block');
-});
+
+// document.querySelector('#dropbtn1').addEventListener('click', function() {
+//   document.querySelector('#droplist1').classList.toggle('is-open_block');
+// });
+
+// document.querySelector('#dropbtn2').addEventListener('click', function() {
+//   document.querySelector('#droplist2').classList.toggle('is-open_block');
+// });
+
+// document.querySelector('#dropbtn3').addEventListener('click', function() {
+//   document.querySelector('#droplist3').classList.toggle('is-open_block');
+// });
+
+// document.querySelector('#dropbtn4').addEventListener('click', function() {
+//   document.querySelector('#droplist4').classList.toggle('is-open_block');
+// });
+
+// document.querySelector('#dropbtn5').addEventListener('click', function() {
+//   document.querySelector('#droplist5').classList.toggle('is-open_block');
+// });
